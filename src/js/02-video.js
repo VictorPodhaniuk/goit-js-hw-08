855cd6d6845adc1b07b841a2864d64d485b0e577
VictorPodhaniuk <thevipo777@gmail.com>
2023-02-27 17:23:15 +0200

    corr. lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
 import Player from '@vimeo/player';
-import { throttle } from 'lodash.throttle';
+import { throttle } from 'lodash';
 
   const iframe = document.querySelector('iframe');
   const player = new Player(iframe);
 
-    player.on('timeupdate', throttle(e => {
-      localStorage.setItem('videoplayer-current-time', e.seconds);
+  player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
     }, 1000)
     );
 
